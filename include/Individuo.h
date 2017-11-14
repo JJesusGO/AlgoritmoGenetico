0ec23b11cf93bcd97afa6b41417a12ff96c9fb7f
AlexisCL31 <alex_cordero31@hotmail.com>
2017-11-14 11:12:00 -0600

    Individuo

--- include/Individuo.h
+++ include/Individuo.h
@@ -1,6 +1,8 @@
 #pragma once
 #include<cstdlib>
 #include"Random.h"
+#include "Binario.h"
+
 class Individuo
 {
 public:
@@ -8,27 +10,30 @@ public:
 	~Individuo();
 
 	void IniciatIndividuo(void);
-	const bool GetGen(int gen);
+	const bool *GetGen(int gen);
 	void MostrarIndividuo(int in);
 
 	void SetIndividuo(void);
-	void SetIndividuo(Individuo NewInndividuo);
-	void SetFiness(float valor);
+	void SetIndividuo(Individuo &NewInndividuo);
+	void SetFitness(float valor);
 	void SetValorObjetivo(float valor);
 	void SetPorcentaje(float valor);
-	void SetValorR(int i,float valor);
+	void SetCromosoma(int i,bool valor);
+	void InverseCromosoma(int i);
 
 	const inline float  GetFitness(int i) const { return ValorR[i]; }
 	const inline float  GetFitness(int i) const { return ValorR[i]; }
-	const inline bool  GetCromosoma(int i) const { return Cromsoma[i]; }
+	const inline bool  GetCromosoma(int i) const { return Cromosoma[i]; }
 	const inline float  GetFitness() const { return Fitness; }
 	const inline float  GetValorObjetivo() const { return ValorObjetibvo; }
 	const inline float GetPorcentaje() const { return Porcentaje; }
 
+	private:
 
+		void Actualizar();
 
 private:
-	bool *Cromsoma;
+	bool *Cromosoma;
 	int *BitsGen;
 	int *TipoGen;
 	float *ValorR;
--- source/Individuo.cpp
+++ source/Individuo.cpp
@@ -16,7 +16,7 @@ Individuo::Individuo(int *bitsGen, int *tipoGen, int genes)
 		Bits += BitsGen[i];
 	}
 
-	Cromsoma = new bool[Bits];
+	Cromosoma = new bool[Bits];
 	ValorObjetibvo = 0;
 	Fitness = 0;
 	Porcentaje = -1;
@@ -24,7 +24,7 @@ Individuo::Individuo(int *bitsGen, int *tipoGen, int genes)
 }
 Individuo::~Individuo()
 {
-	delete[] Cromsoma;
+	delete[] Cromosoma;
 	delete[] BitsGen;
 	delete[] TipoGen;
 
@@ -34,18 +34,19 @@ void Individuo::IniciatIndividuo()
 	bool aleatorio;
 	for (unsigned int i = 0;i < Bits;i++)
 	{
-		aleatorio = Random::NextBoolean;
-		Cromsoma[i] = aleatorio;	
+		aleatorio = Random::NextBoolean();
+		Cromosoma[i] = aleatorio;	
 	}
+	Actualizar();
 }
-const bool Individuo::GetGen(int gen)
+const bool* Individuo::GetGen(int gen)
 {
 	int suma = 0;
 	for (unsigned int i = 0;i < gen;i++)
 	{
 		suma += BitsGen[i];
 	}
-	return Cromsoma + suma;
+	return Cromosoma + suma;
 }
 void Individuo::MostrarIndividuo(int in)
 {
@@ -53,16 +54,14 @@ void Individuo::MostrarIndividuo(int in)
 	printf("\nIndividuo[%i]: ", in);
 	for (unsigned int i = 0;i < Bits;i++)
 	{
-		printf("%i", Cromsoma[i]);
+		printf("%i", Cromosoma[i]);
 	}
 	for (unsigned int i = 0;i < Genes;i++)
 	{
-		printf("\nGen[%i]: ", i + 1);
-		for (unsigned int j = GetGen(i);j < BitsGen[i];j++)
+		const bool *codigo = GetGen(i);
+		for (int j = 0;j < BitsGen[i];j++)
+			printf("%d", codigo[j]);
 
-		{
-			printf("%i", Cromsoma[j]);
-		}
 	}
 	printf("\nValor Decodificado:");
 	for (unsigned int i = 0;i < Genes;i++)
@@ -84,11 +83,11 @@ void Individuo::SetIndividuo(void)
 		ValorR[i] = 0;
 	}
 }
-void Individuo::SetIndividuo(Individuo NewInndividuo)
+void Individuo::SetIndividuo(Individuo &NewInndividuo)
 {
 	for (unsigned int i = 0;i < Bits;i++)
 	{
-		Cromsoma[i] = NewInndividuo.Cromsoma[i];
+		Cromosoma[i] = NewInndividuo.Cromosoma[i];
 	}
 	for (unsigned int i = 0;i < Genes;i++)
 	{
@@ -97,12 +96,14 @@ void Individuo::SetIndividuo(Individuo NewInndividuo)
 	Fitness = NewInndividuo.Fitness;
 	ValorObjetibvo = NewInndividuo.ValorObjetibvo;
 	Porcentaje = NewInndividuo.Porcentaje;
+
+	Actualizar();
 }
 void Individuo::SetValorObjetivo(float valor)
 {
 	ValorObjetibvo = valor;
 }
-void Individuo::SetFiness(float valor)
+void Individuo::SetFitness(float valor)
 {
 	Fitness = valor;
 }
@@ -110,7 +111,26 @@ void Individuo::SetPorcentaje(float valor)
 {
 	Porcentaje = valor;
 }
-void Individuo::SetValorR(int i,float valor)
+void Individuo::SetCromosoma(int i, bool valor)
 {
-	ValorR[i] = valor;
+	Cromosoma[i] = valor;
+}
+void Individuo::InverseCromosoma(int i)
+{
+	if (Cromosoma[i] == true)
+	{
+		Cromosoma[i]=false;
+	}
+	else
+	{
+		Cromosoma[i] = true;
+	}	
+}
+void Individuo::Actualizar()
+{
+	for (int i = 0;i < Genes;i++) 
+	{
+		Binario bin(TipoGen[i]);
+		ValorR[i] = bin.GetNumero((bool*)GetGen(i), BitsGen[i]);
+	}
 }
